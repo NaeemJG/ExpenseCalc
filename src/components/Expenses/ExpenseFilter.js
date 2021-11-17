c3bcd0ca7435469edb6fa45b405c3ef34642dd88
Jamil Naeem <jamil.naeem@bylight.com>
2021-11-17 16:01:17 -0500

    saving changes

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+import './ExpenseFilter.css';
+
+const ExpensesFilter = (props) => {
+    const [selectedDate, setSelectedDate] = useState('')
+
+    const onDateSelectionHandler = (e) => {
+        console.log('things are being selected')
+        setSelectedDate(e.target.value)
+    }
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={onDateSelectionHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,22 @@
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const onSaveExpenseHandler = (enteredExpense) => {
+        const expenseData = {
+            ...enteredExpense,
+            id: Math.random().toString(),
+            jamil: 'naeem'
+        }
+
+        props.onAddExpense(expenseData)
+    }
+
+   
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={onSaveExpenseHandler} />
         </div>
     )
 }
